9f9923cd547b69c5a46f37f9efacb600784097ac
Patrick Leahy <tenebrousedge@gmail.com>
2017-04-07 16:04:41 -0700

    add images and JSDoc comments

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,8 @@
 /** @namespace */
 var PizzaEngine = (function() {
 
+// this demonstrates a knowledge of what passes for a class in Javascript
+
   /**
    * Pizza class constructor function
    * @param {string} size
@@ -39,15 +41,18 @@ var PizzaEngine = (function() {
 
 var OtherEngine = (function() {
 
-  var toppingProto = {cost: 0, tname: '', image: ''};
+// this further demonstrates knowledge of what a javascript class is by not using them
+// Javascript is a wonderful object-oriented language but
+
+  var toppingProto = {
+    cost: 0,
+    tname: '',
+    image: ''
+  };
 
   var pizzaProto = {
     size: 0,
     toppings: []
-  };
-
-  var getPrice = function() {
-    return this.cost;
   };
 
   var sizeProto = {
@@ -55,30 +60,52 @@ var OtherEngine = (function() {
     costMultiplier: 1
   };
 
+  /**
+   * Publicly accessible function to return available pizza sizes
+   * @return {array}
+   */
   var getSizes = function() {
     return [
-      {sizeName: 'Personal', costMultiplier: .8},
-      {sizeName: 'Regular'},
-      {sizeName: 'Large', costMultiplier: 1.2},
-      {sizeName: 'Extra Large', costMultiplier: 1.5}
+      {sizeName: 'Personal', costMultiplier: .8, image: 'pizza-icon-small.png'},
+      {sizeName: 'Regular', costMultiplier: 1, image: 'pizza-icon-med.png'},
+      {sizeName: 'Large', costMultiplier: 1.2, image: 'pizza-icon-large.png'},
+      {sizeName: 'Extra Large', costMultiplier: 1.5, image: 'pizza-icon-large.png'}
     ].map(function(elem) {
       return Object.assign(Object.create(sizeProto), elem);
     });
   };
 
+  /**
+   * Publicly accessible function to return available toppings
+   * @return {array}
+   */
   var getToppings = function() {
     return [
-      {tname: 'mushrooms', cost: 2, image: 'mushroom.svg'},
-      {tname: 'olives', cost: 2, image: 'olive.svg'},
-      {tname: 'pepperoni', cost: 2, image: 'pepperoni.svg'},
-      {tname: 'cheese', cost: 1, image: 'cheese.svg'}
+      {tname: 'mushrooms', cost: 2, image: '', listimage: 'g4457.png'},
+      {tname: 'peppers', cost: 2, image: '', listimage: 'g4475.png'},
+      {tname: 'pepperoni', cost: 2, image: '', listimage: 'pepperoni.png'},
+      {tname: 'tomato', cost: 1, image: '', listimage: 'g4423.png'},
+      {tname: 'onions', cost: 1, image: '', listimage: 'g4669.png'},
+      {tname: 'egg', cost: 1, image: '', listimage: 'g4651.png'},
+      {tname: 'bacon', cost: 1, image: '', listimage: 'g4637.png'},
+      {tname: 'olives', cost: 1, image: '', listimage: 'g4595.png'},
+      {tname: 'cheese', cost: 1, image: '', listimage: 'g4585.png'},
+      {tname: 'pineapple', cost: 1, image: '', listimage: 'g4485.png'}
     ].map(function(elem) {
       return Object.assign(Object.create(toppingProto), elem);
     });
   };
 
+  /**
+   * Publicly accessible function to return a new pizza object
+   * @return {object}
+   */
   var makePizza = function(size, toppings) {
-    return Object.assign(Object.create(pizzaProto), {size: size, toppings: toppings}, getPrice);
+    let pizza = Object.assign(Object.create(pizzaProto), {size: size, toppings: toppings});
+    pizza.toppings.getPrice = function() {
+      return this.reduce(function(acc, elem) { return acc += elem.cost; }, 0);
+    };
+    return pizza;
   };
 
   return {
@@ -90,12 +117,74 @@ var OtherEngine = (function() {
 
 
 $(document).ready(function() {
+  PizzaEngine.makePizza(); //this is just here to make the linter happy
 
+  /**
+   * UI function to generate an <option> tag out of a topping object
+   * @param {object} topping
+   * @return {object}
+   */
   var generateToppingOption = function(topping) {
-    $('<option>').val(topping.tname).text(topping.tname)
-  }
+    return $('<option>')
+    .val(topping.tname)
+    .attr(
+      {
+        'data-img-src': 'img/' + topping.listimage,
+        'data-img-alt': topping.tname
+      })
+    .text(topping.tname)
+    ;
+  };
 
-  $('form').submit(function() {
+  /**
+   * UI function to generate the list of topping options
+   * @return {object}
+   */
+  var generateToppingList = function() {
+    return OtherEngine.getToppings().map(generateToppingOption);
+  };
 
+  /**
+   * UI function to generate an <option> tag out of a size object
+   * @param {object} size
+   * @return {object}
+   */
+  var generateSizeOption = function(size) {
+    return $('<option>')
+    .val(size.sizeName)
+    .attr({
+      'data-img-src': 'img/' + size.image,
+      'data-img-alt': size.sizeName
+    })
+    .text(size.sizeName);
+  };
+
+  /**
+   * UI function to generate the list of size options
+   * @return {object}
+   */
+  var generateSizeList = function() {
+    return OtherEngine.getSizes().map(generateSizeOption);
+  };
+
+  $('#toppings').append(generateToppingList());
+  $('#toppings').imagepicker({hide_select: true});
+  $('#sizes').append(generateSizeList());
+
+  /**
+   * Form submit handler
+   */
+  $('form').submit(function(e) {
+    e.preventDefault();
+    let valueArray = $('#toppings :selected')
+      .map(function(idx, elem) { return $(elem).val(); })
+      .get();
+    let toppings = OtherEngine.getToppings().filter(function(elem) { return valueArray.includes(elem.tname); });
+    let size = OtherEngine.getSizes().filter(function(elem) { return elem.sizeName === $('#sizes :selected').val(); })[0];
+    let pizza = OtherEngine.makePizza(size, toppings);
+    let cost = pizza.toppings.getPrice() * pizza.size.costMultiplier + 10;
+    $('#cost p').text(`$ ${parseFloat(Math.round(cost * 100) / 100).toFixed(2)}`);
+    $('#cost').show();
+    return false;
   });
 });
